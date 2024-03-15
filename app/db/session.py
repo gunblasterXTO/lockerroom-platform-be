e64b43c266dce0192c63770d16bed1011b4ee5a2
gunblasterXTO <naufal.abdila@outlook.com>
2024-03-16 05:25:36 +0700

    feat - update db abstraction

--- app/db/session.py
+++ app/db/session.py
@@ -7,9 +7,13 @@ from app.core.settings import Settings
 
 class Database:
     def __init__(self) -> None:
-        self._engine = create_engine(
-            url=Settings.DB_URL, connect_args={"check_same_thread": False}
-        )  # connect_args for sqlite only
+        if Settings.DB_DIALECT == "sqlite":
+            self._engine = create_engine(
+                url=Settings.DB_URL,
+                connect_args={"check_same_thread": False},  # sqlite only
+            )
+        else:
+            self._engine = create_engine(url=Settings.DB_URL)
         self.session = sessionmaker(
             autocommit=False, autoflush=True, bind=self._engine
         )
